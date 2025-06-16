7aaf4e88dcef47ab937c6eca507e20e7bab341c5
Olotuah <gbengaolotuah@gmail.com>
2025-06-16 12:08:41 +0100

    Ready for production: use live API for mobile

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { getToken } from '../services/authService';
-
-const PrivateRoute = ({ children }) => {
-  return getToken() ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
