5c38395d87686b46ff71544447edc48058296561
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-01 13:24:50 +0100

    Add admin setup routes to server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import transactionRoutes from './routes/transactions.js';
+import adminSetupRoutes from "./routes/adminSetupRoutes.js";
 import accountRoutes from './routes/account.js';
 import authRoutes from './routes/auth.js';
 import transferRoutes from "./routes/transferRoutes.js";
@@ -14,6 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/transfers", transferRoutes);
+app.use("/api/admin", adminSetupRoutes);
 app.use("/api/auth", authRoutes); // ✅ check this too
 app.use('/api/accounts', accountRoutes);
 app.use("/api/transactions", transactionRoutes); // ✅ your error is here
