c4a1e1f0465557a71991afd1d45b1f0981af9b70
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-02 21:02:07 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import transactionRoutes from './routes/transactions.js';
 import adminSetupRoutes from "./routes/adminSetupRoutes.js";
 import accountRoutes from './routes/account.js';
+import userRoutes from "./routes/userRoutes.js";
 import authRoutes from './routes/auth.js';
 import transferRoutes from "./routes/transferRoutes.js";
 import tempAdminRoute from "./routes/tempAdminRoute.js";
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/transfers", transferRoutes);
 app.use("/api/admin", adminSetupRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes); // ✅ check this too
 app.use('/api/accounts', accountRoutes);
 app.use("/api", tempAdminRoute);
