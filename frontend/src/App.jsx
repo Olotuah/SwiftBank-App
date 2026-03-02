91c889ef698504465910c5bd74794fc51dad3771
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-02 19:14:07 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,30 +9,35 @@ import Transfer from "./pages/Transfer";
 import Bills from "./pages/Bills";
 import Cards from "./pages/Cards";
 import Profile from "./pages/Profile";
+import SessionTimeout from "./components/SessionTimeout";
 import TopUp from "./pages/TopUp";
 import FxSales from "./pages/FxSales";
 import BuyData from "./pages/BuyData";
 import NearMe from "./pages/NearMe";
 
-
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/transactions" element={<Transactions />} />
-      <Route path="/admin/transfers" element={<AdminTransfers />} />
-      <Route path="/transfer" element={<Transfer />} />
-      <Route path="/bills" element={<Bills />} />
-      <Route path="/cards" element={<Cards />} />
-      <Route path="/topup" element={<TopUp />} />
-      <Route path="/fx" element={<FxSales />} />
-      <Route path="/buy-data" element={<BuyData />} />
-      <Route path="/near-me" element={<NearMe />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
+    <>
+      {/* ✅ Global inactivity watcher */}
+      <SessionTimeout idleMinutes={10} />
+
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/admin/transfers" element={<AdminTransfers />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/bills" element={<Bills />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/topup" element={<TopUp />} />
+        <Route path="/fx" element={<FxSales />} />
+        <Route path="/buy-data" element={<BuyData />} />
+        <Route path="/near-me" element={<NearMe />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
 
