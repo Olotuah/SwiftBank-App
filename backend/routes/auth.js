f60ae9fe0633a5541df5514ece26eeedabce0b6d
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-02 20:05:46 +0100

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import protect from "../middleware/authMiddleware.js";
+import { setTransferPin, getMe } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+router.get("/me", protect, getMe);
+router.post("/pin", protect, setTransferPin);
+
 
 export default router;
