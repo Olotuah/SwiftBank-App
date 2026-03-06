163bbb71d4e52ded0c67109792c1a9e94135cb61
Gbenga Olotuah <74839657+Olotuah@users.noreply.github.com>
2026-03-06 22:43:06 +0100

    Update Transaction.js

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,15 +1,23 @@
 // models/Transaction.js
 import mongoose from "mongoose";
 
-const TransactionSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  type: { type: String, enum: ["Credit", "Debit"], required: true },
-  amount: { type: Number, required: true },
-  transferRef: { type: String },
-  counterparty: { type: String }, // receiver account or sender account
-  description: { type: String, required: true },
-  status: { type: String, default: "Completed" },
-  timestamp: { type: Date, default: Date.now },
-}, { timestamps: true });
+const TransactionSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    type: { type: String, enum: ["Credit", "Debit"], required: true },
+    amount: { type: Number, required: true },
+    transferRef: { type: String },
+    counterparty: { type: String },
+    fromAccount: {
+      type: String,
+      enum: ["Main Account", "Savings", "Dollar Wallet"],
+      default: "Main Account",
+    },
+    description: { type: String, required: true },
+    status: { type: String, default: "Completed" },
+    timestamp: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Transaction", TransactionSchema);
